dd21a21a30df12023c5c56f990eea1be3ffeaebb
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2022-06-13 17:45:08 +0900

    Add files via upload

---
+++ 120Triangle.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle);
+    // O(N+M) N==triangle.size()+triangle[row++].size(), M==triangle[triangle.size()-1].size()
+    int minimumTotalBottomUp(vector<vector<int>>& triangle);
+};
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    vector<vector<int>> Triangle = { {2},{3,4},{6,5,7},{4,1,8,3} };
+    Solution solution;
+	cout << solution.minimumTotalBottomUp(Triangle);
+
+    return 0;
+}
+
+int Solution::minimumTotal(vector<vector<int>>& triangle)
+{
+    int MinimumPathSum = triangle[0][0];
+    const int TriangleRowSize = (int)triangle.size();
+	if (TriangleRowSize == 1)
+    {
+        return MinimumPathSum;
+    }
+    else if(TriangleRowSize == 2)
+    {
+        MinimumPathSum += min(triangle[1][0], triangle[1][1]);
+        return MinimumPathSum;
+    }
+
+    triangle[1][0] += triangle[0][0];
+    triangle[1][1] += triangle[0][0];
+	for (int row = 2; row < TriangleRowSize; row++)
+    {
+		for (int col = 0; col < (int)triangle[row].size(); col++)
+	    {
+			if (col == 0)
+            {
+                triangle[row][col] += triangle[row - 1][col];
+            }
+			else if (col == (int)triangle[row].size() - 1)
+            {
+                triangle[row][col] += triangle[row - 1][col - 1];
+            }
+            else
+            {
+                triangle[row][col] += min(triangle[row - 1][col - 1], triangle[row - 1][col]);
+            }
+	    }
+    }
+    MinimumPathSum = *min_element(triangle[TriangleRowSize - 1].begin(), triangle[TriangleRowSize - 1].end());
+
+    return MinimumPathSum;
+}
+
+int Solution::minimumTotalBottomUp(vector<vector<int>>& triangle)
+{
+	for (int row = (int)triangle.size() - 2; row >= 0; row--)
+	{
+		for (int col = 0; col < (int)triangle[row].size(); col++)
+		{
+            triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1]);
+		}
+	}
+    return triangle[0][0];
+}
+
