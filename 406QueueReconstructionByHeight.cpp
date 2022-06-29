601c2a7636462f19f3aafa9576f3910ea563d8be
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2022-06-29 23:34:00 +0900

    comment++

--- 406QueueReconstructionByHeight.cpp
+++ 406QueueReconstructionByHeight.cpp
@@ -1,3 +1,5 @@
+// people[i]는 people의 키 hi와 자신의 앞에 있는 people중 자신보다 크거나 같은 people의 수 ki로 이루어져있다.
+// 올바르게 정렬하라
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -6,8 +8,7 @@ using namespace std;
 class Solution {
 public:
 	static bool compare(vector<int>& v1, vector<int>& v2);
-	vector<vector<int>> reconstructQueue(vector<vector<int>>& people);
-	void Update(vector<int>& Person, const int& begin);
+	vector<vector<int>> reconstructQueue(vector<vector<int>>& people); //O(NlogN)+O(N^2)
 	void Print2DVector(const vector<vector<int>>& people);
 };
 
@@ -25,10 +26,10 @@ int main(void)
 vector<vector<int>> Solution::reconstructQueue(vector<vector<int>>& people)
 {
 	int PeopleSize = (int)people.size();
-	sort(people.begin(), people.end(), compare);
+	sort(people.begin(), people.end(), compare); // O(NlogN)
 
 	vector<vector<int>> result(PeopleSize, vector<int>());
-	for (int i = 0; i < PeopleSize; i++)
+	for (int i = 0; i < PeopleSize; i++) // O(N^2)
 	{
 		int count = 0;
 		int j;
@@ -47,8 +48,8 @@ vector<vector<int>> Solution::reconstructQueue(vector<vector<int>>& people)
 
 bool Solution::compare(vector<int>& v1, vector<int>& v2)
 {
-	if (v1[0] != v2[0]) return v1[0] < v2[0];
-	else return v1[1] > v2[1];
+	if (v1[0] != v2[0]) return v1[0] < v2[0]; // 키 순서로 오름차순
+	else return v1[1] > v2[1]; // 키가 같다면 ki의 내림차순
 }
 
 void Solution::Print2DVector(const vector<vector<int>>& people)
@@ -58,3 +59,5 @@ void Solution::Print2DVector(const vector<vector<int>>& people)
 		cout << e[0] << " " << e[1] << '\n';
 	}
 }
+// hi 혹은 ki를 기준으로 정렬한 뒤 다시 한번더 정렬을 해야됐는데 이부분이 조금 까다로웠다.
+// 이진 탐색을 이용해서 최적화도 가능할 것 같은데 조금 더 생각해봐야겠다.
