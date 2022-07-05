823d7cc3619015d861068165d4f9cf41a6702840
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2022-07-05 20:44:50 +0900

    comment++

--- 128LongestConsecutiveSequence.cpp
+++ 128LongestConsecutiveSequence.cpp
@@ -1,3 +1,4 @@
+// 배열이 주어졌을 때 가장 긴 연속 수열의 길이를 구하여라
 #include <iostream>
 #include <vector>
 #include <unordered_set>
@@ -22,9 +23,9 @@ int main(void)
 
 int Solution::longestConsecutive(vector<int>& nums)
 {
-	unordered_set<int> ToSearchSet(nums.begin(), nums.end());
+	unordered_set<int> ToSearchSet(nums.begin(), nums.end()); // O(N)
 	int LengthTheLongest = 0;
-	for (const int& num : ToSearchSet)
+	for (const int& num : ToSearchSet) //O(N)
 	{
 		if (ToSearchSet.count(num - 1))
 		{
@@ -42,3 +43,7 @@ int Solution::longestConsecutive(vector<int>& nums)
 	}
 	return LengthTheLongest;
 }
+// 가장빠른 방법은 정렬을 한 뒤 연속 수열을 찾는 것이었다.
+// 내가 푼 방식은 unordered_set에 입력값을 넣고 연속수열을 찾는 것이었다.
+// unordered_set은 입력과 탐색이 O(1)의 속도로 상당히 빠르기 때문에 가장 적합하다고 생각했다.
+// O(N)시간 안에 해결해야 하는 문제라서 정렬 없이 한번의 탐색으로 해야 된다 생각했는데 뒷통수 맞은 기분이었다.
