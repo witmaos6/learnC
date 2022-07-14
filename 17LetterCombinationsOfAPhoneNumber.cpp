58bef439bd360e907a45c4fb8c06c771058bbf6a
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2022-07-14 16:59:08 +0900

    comment++

--- 17LetterCombinationsOfAPhoneNumber.cpp
+++ 17LetterCombinationsOfAPhoneNumber.cpp
@@ -1,3 +1,4 @@
+// PhoneNumber가 있다. digits가 주어지면 PhoneNumber에 있는 가능한 조합을 순서에 상관없이 출력해라.
 #include <iostream>
 #include <vector>
 #include <string>
@@ -74,3 +75,7 @@ void Solution::PrintVector(const vector<string>& vs_vector)
 		cout << s << ' ';
 	}
 }
+// 시간 복잡도는 digits의 길이를 N, 숫자의 알파벳 개수를 M이라고 했을 때 (M^N)*N이 된다. M은 최대 4이기 때문에 O((4^N)*N)이다.
+// digits의 길이가 일정하다면 for문 중첩으로 풀 수 있었지만 일정하지 않았기 때문에 재귀함수를 사용했다.
+// DFS를 어떤식으로 구현해야 할지 모르겠어서 discuss를 참고 했다.
+// 인수가 5개나 되서 줄일 필요가 있다.
