675b2cb01f5295565bbc089ac8ee0e87d38aa1c9
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2022-08-08 14:42:37 +0900

    comment++

--- 300LongestIncreasingSubsequence.cpp
+++ 300LongestIncreasingSubsequence.cpp
@@ -1,3 +1,4 @@
+// 가장 긴 오름차순 Subsequence를 구하라, Subsequence는 element를 삭제한 경우는 포함되나 변경은 포함하지 않는다.
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -21,7 +22,7 @@ int main(void)
 	return 0;
 }
 
-int Solution::lengthOfLIS(vector<int>& nums)
+int Solution::lengthOfLIS(vector<int>& nums) //O(N^2)
 {
 	int NumsSize = static_cast<int>(size(nums));
 	vector<int> dp(NumsSize, 1);
@@ -43,7 +44,7 @@ int Solution::lengthOfLIS(vector<int>& nums)
 int Solution::LengthOfLISBinarySearch(vector<int>& nums)
 {
 	int LIS = 0;
-	for (int& current : nums)
+	for (int& current : nums) // O(N)
 	{
 		if (LIS == 0 || nums[LIS - 1] < current)
 		{
@@ -51,8 +52,10 @@ int Solution::LengthOfLISBinarySearch(vector<int>& nums)
 		}
 		else
 		{
-			*lower_bound(begin(nums), begin(nums) + LIS, current) = current;
+			*lower_bound(begin(nums), begin(nums) + LIS, current) = current; // O(logN)
 		}
 	}
 	return LIS;
 }
+//처음에는 재귀함수로 시도했는데 오차가 발생했다. discuss를 참고하여 return 값인 LIS를 따로 저장하는 방식을 알아내서 풀었다.
+// BinarySearch로 푸는 방식은 잘 이해가 안되었지만 NlogN의 시간복잡도를 보장하는 함수라서 가져와봤다.
