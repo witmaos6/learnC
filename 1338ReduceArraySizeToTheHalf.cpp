e7b25c14e198d7b8fd908ec27cf733191fba250b
witmaos6 <62473633+witmaos6@users.noreply.github.com>
2022-08-19 02:01:53 +0900

    comment++

--- 1338ReduceArraySizeToTheHalf.cpp
+++ 1338ReduceArraySizeToTheHalf.cpp
@@ -1,3 +1,4 @@
+// 중복된 정수를 최소 개수로 제거하여 vector의 크기를 절반이하로 
 #include <iostream>
 #include <vector>
 #include <map>
@@ -31,9 +32,9 @@ int Solution::minSetSize(vector<int>& arr)
 {
 	map<int, int>Subset;
 	priority_queue<int> pq;
-	int ArrHalfSize = static_cast<int>(size(arr)) / 2;
+	int ArrHalfSize = static_cast<int>(size(arr) + 1) >> 1;
 
-	for (int& num : arr)
+	for (int& num : arr) // O(NlogN)
 	{
 		if (Subset.find(num) == Subset.end())
 		{
@@ -44,12 +45,12 @@ int Solution::minSetSize(vector<int>& arr)
 			Subset[num]++;
 		}
 	}
-	for (auto& e : Subset)
+	for (auto& e : Subset) // O(NlogN)
 	{
 		pq.push(e.second);
 	}
 	int CountingSubset = 0;
-	while (!pq.empty())
+	while (!pq.empty()) // O(NlogN)
 	{
 		ArrHalfSize -= pq.top();
 		CountingSubset++;
@@ -68,20 +69,20 @@ int Solution::MinSetSizeUseBucketSort(vector<int>& arr)
 	int CountingSubsetDelete = 0;
 	int HalfSize = static_cast<int>(size(arr) + 1) >> 1;
 
-	for (auto& num : arr)
+	for (auto& num : arr) // O(N)
 	{
 		Frequency[num]++;
 	}
-	for (int i = 1; i < MaxValue; i++)
+	for (int i = 1; i < MaxValue; i++) // O(N)
 	{
 		if (Frequency[i])
 		{
 			Bucket[Frequency[i]]++;
 		}
 	}
-	for (int f = MaxValue - 1; HalfSize > 0; f--)
+	for (int f = MaxValue - 1; HalfSize > 0; f--) // O(N)
 	{
-		while (Bucket[f] && HalfSize > 0)
+		while (Bucket[f] > 0 && HalfSize > 0)
 		{
 			HalfSize -= f;
 			Bucket[f]--;
@@ -90,3 +91,6 @@ int Solution::MinSetSizeUseBucketSort(vector<int>& arr)
 	}
 	return CountingSubsetDelete;
 }
+// 첫번째 방식은 map을 이용해서 중복된 정수와 개수를 찾아내고 우선순위큐를 이용해서 중복된 정수의 개수를 큰 순서로 빼는 것이다.
+// 두번째 방식은 vector가 랜덤액세스가 빠르다는 성질을 이용한 것이다. 속도는 빨라도 메모리 낭비는 심할것이라 생각했는데 메모리 사용량도 더 낮았다.
+// 첫번째 방식의 시간과 메모리 사용량은 431ms 80mb고 두번째 방식은 104ms 56mb이다.
