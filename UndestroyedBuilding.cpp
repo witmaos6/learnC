b29922623c3b8a0bc3ee732ec1f58b6d994e358c
geonhee <62473633+witmaos6@users.noreply.github.com>
2022-10-06 20:43:17 +0900

    comment++

--- UndestroyedBuilding.cpp
+++ UndestroyedBuilding.cpp
@@ -38,14 +38,14 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill)
 	size_t ColSize = board[0].size();
 	vector<vector<int>> TotalRecord(RowSize + 1, vector<int>(ColSize + 1));
 
-	for (vector<int>& Record : skill)
+	for (vector<int>& Record : skill) // O(S) S == skill.size();
 	{
 		Memoization(Record, TotalRecord);
 	}
 
 	PrefixSum(TotalRecord, RowSize, ColSize);
 	
-	for (size_t row = 0; row < RowSize; row++)
+	for (size_t row = 0; row < RowSize; row++) // O(R*C) R==RowSize, C == ColSize
 	{
 		for (size_t col = 0; col < ColSize; col++)
 		{
@@ -59,7 +59,7 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill)
 	return answer;
 }
 
-void Memoization(const vector<int>& Record, vector<vector<int>>& TotalRecord)
+void Memoization(const vector<int>& Record, vector<vector<int>>& TotalRecord) //O(1)
 {
 	int Degree = Record[5];
 	if (Record[0] == 1)
@@ -74,7 +74,7 @@ void Memoization(const vector<int>& Record, vector<vector<int>>& TotalRecord)
 	TotalRecord[Record[3] + 1][Record[2]] -= Degree;
 }
 
-void PrefixSum(vector<vector<int>>& TotalRecord, const size_t& RowSize, const size_t& ColSize)
+void PrefixSum(vector<vector<int>>& TotalRecord, const size_t& RowSize, const size_t& ColSize) // O(R*C) R==RowSize, C == ColSize
 {
 	for (size_t row = 0; row <= RowSize; row++)
 	{
@@ -103,3 +103,5 @@ void PrintMatrix(vector<vector<int>>& Matrix)
 		cout << '\n';
 	}
 }
+// PrefixSum 은 누적합을 계산하는 방법 중 하나다. Record를 읽을 때마다 TotalRecord에 영향이 가는 모든 셀을 바꿔주는 것이 아닌 4개의 지점에다가 표시만 해놓는다.
+// 표시된 부분을 읽어서 부분합을 계산하는 방식이다. 
