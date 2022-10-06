ae2ebd75e88ba0822afae7e3c97f3b69a3b35da3
geonhee <62473633+witmaos6@users.noreply.github.com>
2022-10-06 20:38:05 +0900

    Add files via upload

---
+++ UndestroyedBuilding.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<vector<int>> skill);
+void Memoization(const vector<int>& Record, vector<vector<int>>& TotalRecord);
+void PrefixSum(vector<vector<int>>& TotalRecord, const size_t& RowSize, const size_t& ColSize);
+void PrintMatrix(vector<vector<int>>& Matrix);
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	vector<vector<int>> Board = {
+		{5,5,5,5,5},
+		{5,5,5,5,5},
+		{5,5,5,5,5},
+		{5,5,5,5,5}
+	};
+	vector<vector<int>> Skill = {
+		{1,0,0,3,4,4},
+		{1,2,0,2,3,2},
+		{2,1,0,3,1,2},
+		{1,0,1,3,3,1}
+	};
+
+	cout << solution(Board, Skill) << '\n';
+
+	return 0;
+}
+
+int solution(vector<vector<int>> board, vector<vector<int>> skill)
+{
+	int answer = 0;
+	size_t RowSize = board.size();
+	size_t ColSize = board[0].size();
+	vector<vector<int>> TotalRecord(RowSize + 1, vector<int>(ColSize + 1));
+
+	for (vector<int>& Record : skill)
+	{
+		Memoization(Record, TotalRecord);
+	}
+
+	PrefixSum(TotalRecord, RowSize, ColSize);
+	
+	for (size_t row = 0; row < RowSize; row++)
+	{
+		for (size_t col = 0; col < ColSize; col++)
+		{
+			if (board[row][col] + TotalRecord[row][col] > 0)
+			{
+				answer++;
+			}
+		}
+	}
+
+	return answer;
+}
+
+void Memoization(const vector<int>& Record, vector<vector<int>>& TotalRecord)
+{
+	int Degree = Record[5];
+	if (Record[0] == 1)
+	{
+		Degree *= -1;
+	}
+
+	TotalRecord[Record[1]][Record[2]] += Degree;
+	TotalRecord[Record[3] + 1][Record[4] + 1] += Degree;
+
+	TotalRecord[Record[1]][Record[4] + 1] -= Degree;
+	TotalRecord[Record[3] + 1][Record[2]] -= Degree;
+}
+
+void PrefixSum(vector<vector<int>>& TotalRecord, const size_t& RowSize, const size_t& ColSize)
+{
+	for (size_t row = 0; row <= RowSize; row++)
+	{
+		for (size_t col = 0; col < ColSize; col++)
+		{
+			TotalRecord[row][col + 1] += TotalRecord[row][col];
+		}
+	}
+	for (size_t col = 0; col <= ColSize; col++)
+	{
+		for (size_t row = 0; row < RowSize; row++)
+		{
+			TotalRecord[row + 1][col] += TotalRecord[row][col];
+		}
+	}
+}
+
+void PrintMatrix(vector<vector<int>>& Matrix)
+{
+	for(vector<int>& row : Matrix)
+	{
+		for(int& col : row)
+		{
+			cout << col << ' ';
+		}
+		cout << '\n';
+	}
+}
