26d0e43a70e22e5c38cd113b102f46f74a5b2ed9
geonhee <62473633+witmaos6@users.noreply.github.com>
2022-10-07 23:43:55 +0900

    do~while문을 이용하여 실행 및 예외처리 추가

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -4,33 +4,65 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    LinkedList linkedList;
-    List list;
-    linkedList.InsertTail(list, 1);
-    linkedList.Traverse(list);
-    linkedList.InsertTail(list, 5);
-    linkedList.InsertTail(list, 7);
-    linkedList.Traverse(list);
-    linkedList.InsertThis(list, 6, 5);
-    linkedList.InsertThis(list, 9, 7);
-    linkedList.Traverse(list);
-    linkedList.InsertTail(list, 10);
-    linkedList.Traverse(list);
-    linkedList.DeleteThis(list, 1);
-    linkedList.Traverse(list);
-    linkedList.DeleteThis(list, 7);
-    linkedList.DeleteThis(list, 5);
-    linkedList.Traverse(list);
-    linkedList.DeleteThis(list, 10);
-    linkedList.Traverse(list);
-    linkedList.InsertTail(list, 15);
-    linkedList.Traverse(list);
+	LinkedList linkedList;
+	List list;
+	int Commad;
 
-    return 0;
+	do
+	{
+		cout << "명령을 입력하세요. \n";
+		cout << "1 : InsertTail\n";
+		cout << "2 : InsertThis\n";
+		cout << "3 : DeleteThis\n";
+		cout << "4 : Traverse\n";
+		cout << "0 : Exit\n";
+		cin >> Commad;
+		switch (Commad)
+		{
+		case 1: int PushValue;
+			cout << "push할 값을 입력하세요: ";
+			cin >> PushValue;
+			linkedList.InsertTail(list, PushValue);
+			break;
+		case 2:
+			if (linkedList.bListEmpty(list))
+			{
+				cout << "List가 비어있습니다. ";
+				break;
+			}
+			int InputValue, AfterValue;
+			cout << "push할 값과 어떤 값 뒤에 넣을 것인지 입력하세요: ";
+			cin >> InputValue >> AfterValue;
+			linkedList.InsertThis(list, InputValue, AfterValue);
+			break;
+		case 3:
+			if(linkedList.bListEmpty(list))
+			{
+				cout << "List가 비어있습니다. ";
+				break;
+			}
+			int DeleteValue;
+			cout << "삭제할 값을 입력하세요: ";
+			cin >> DeleteValue;
+			linkedList.DeleteThis(list, DeleteValue);
+			break;
+		case 4:
+			if (linkedList.bListEmpty(list))
+			{
+				cout << "List가 비어있습니다. ";
+				break;
+			}
+			cout << "리스트를 순회합니다 .\n";
+			linkedList.Traverse(list);
+			break;
+		case 0: cout << "종료합니다. "; break;
+		default: cout << "잘못된 명령입니다. \n"; break;
+		}
+	} while (Commad != 0);
+
+	return 0;
 }
-
-// Node는 중복이 안되므로 private 클래스에 넣음
