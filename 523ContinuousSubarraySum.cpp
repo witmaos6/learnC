029110803e527f0746c2a04ca2465abbff27abf4
geonhee <62473633+witmaos6@users.noreply.github.com>
2022-10-26 17:12:37 +0900

    comment++

--- 523ContinuousSubarraySum.cpp
+++ 523ContinuousSubarraySum.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+// 최소 두개 이상의 nums 값의 합이 k의 배수인 경우가 있다면 true를 반환하라. 0은 k의 배수에 포함된다.
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
@@ -8,7 +9,7 @@ class Solution
 public:
 	bool checkSubarraySum(vector<int>& nums, int k);
 	bool CheckSubarraySumUsePrefixSum(vector<int>& nums, int k);
-	bool bIsLeastTwoElement(int Index, int Range);
+	bool bIsLeastTwoElement(int& FindIndex, int& CurrentIndex);
 };
 
 int main()
@@ -88,7 +89,11 @@ bool Solution::CheckSubarraySumUsePrefixSum(vector<int>& nums, int k)
 	return false;
 }
 
-bool Solution::bIsLeastTwoElement(int Index, int Range)
+bool Solution::bIsLeastTwoElement(int& FindIndex, int& CurrentIndex)
 {
-	return(Index - Range > 1) ? true : false;
+	return(FindIndex - CurrentIndex > 1) ? true : false;
 }
+
+// 처음에는 우선 백트래킹으로 풀었다. 당연히 시간초과였기 때문에 다른방법을 생각했다.
+// 연속하는 서브어레이기 때문에 누적합 알고리즘으로 접근했다. 탐색 횟수는 O(N^2)으로 같았기 때문에 다른 방법을 더 찾아야 했다.
+// 누적합에 Mod k를 해서 키값으로 인덱스를 value값으로 저장한 후 탐색을 시도했다. 시간복잡도는 O(N)으로 축소된다.
