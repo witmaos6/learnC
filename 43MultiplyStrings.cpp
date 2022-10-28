91d39efd2b8aadcd2451fb1026133789485e7036
geonhee <62473633+witmaos6@users.noreply.github.com>
2022-10-28 16:48:29 +0900

    comment++

--- 43MultiplyStrings.cpp
+++ 43MultiplyStrings.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+// 두개의 string num1, num2의 곱셉결과를 string으로 반환하라.
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -38,7 +39,7 @@ string Solution::multiply(string num1, string num2)
 	vector<int> ToNum1 = StringToIntVector(num1, Num1Size);
 	vector<int> ToNum2 = StringToIntVector(num2, Num2Size);
 
-	vector<int> Multiplcation(Num1Size + Num2Size);
+	vector<int> Multiplcation(Num1Size + Num2Size); // N자리수 숫자 * M자리수 숫자는 N+M자리수보다 작거나 같다.
 
 	for (int i = Num1Size - 1; i >= 0; i--)
 	{
@@ -75,3 +76,5 @@ vector<int> Solution::StringToIntVector(string& s, int& sSize)
 	}
 	return ToIntVector;
 }
+// ull(unsigned long long)의 최대값은 1800경 정도 된다. 그래서 그 이하의 자리수는 그냥 ull값으로 전환->곱셈->string 으로 다시 전환 후 반환 알고리즘을 사용했다.
+// ull로 표현이 불가능한 숫자들은 계산하면서 동시에 자리수를 고려하며 계산한 뒤 적절한 위치에 넣어주면 됐다. 자리수를 파악하는 것은 곱셈의 특성을 알면 쉽다.
