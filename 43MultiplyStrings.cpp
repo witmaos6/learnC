bac63fedac9b4027cae87956bab60e3c146cccc3
geonhee <62473633+witmaos6@users.noreply.github.com>
2022-10-28 16:54:18 +0900

    Add LessThanMaxDigit()

--- 43MultiplyStrings.cpp
+++ 43MultiplyStrings.cpp
@@ -2,15 +2,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution
 {
 public:
-	int stringToINT = '0';
 	string multiply(string num1, string num2);
 	vector<int> StringToIntVector(string& s, int& sSize);
+	void LessThanMaxDigit(size_t& i, vector<int>& Result);
 };
 
 int main()
@@ -30,16 +29,12 @@ string Solution::multiply(string num1, string num2)
 {
 	int Num1Size = static_cast<int>(num1.size());
 	int Num2Size = static_cast<int>(num2.size());
-
-	if (Num1Size + Num2Size <= 19)
-	{
-		return to_string(stoull(num1) * stoull(num2));
-	}
+	int MaxDigitsize = Num1Size + Num2Size;
 
 	vector<int> ToNum1 = StringToIntVector(num1, Num1Size);
 	vector<int> ToNum2 = StringToIntVector(num2, Num2Size);
 
-	vector<int> Multiplcation(Num1Size + Num2Size); // N자리수 숫자 * M자리수 숫자는 N+M자리수보다 작거나 같다.
+	vector<int> Multiplcation(MaxDigitsize);
 
 	for (int i = Num1Size - 1; i >= 0; i--)
 	{
@@ -53,10 +48,9 @@ string Solution::multiply(string num1, string num2)
 
 	size_t i = 0;
 	string Result = "";
-	while (Multiplcation[i] == 0)
-	{
-		i++;
-	}
+
+	LessThanMaxDigit(i, Multiplcation);
+	
 	while (i < Multiplcation.size())
 	{
 		Result += to_string(Multiplcation[i]);
@@ -76,5 +70,14 @@ vector<int> Solution::StringToIntVector(string& s, int& sSize)
 	}
 	return ToIntVector;
 }
+
+void Solution::LessThanMaxDigit(size_t& i, vector<int>& Result)
+{
+	while (Result[i] == 0)
+	{
+		i++;
+	}
+}
+
 // ull(unsigned long long)의 최대값은 1800경 정도 된다. 그래서 그 이하의 자리수는 그냥 ull값으로 전환->곱셈->string 으로 다시 전환 후 반환 알고리즘을 사용했다.
 // ull로 표현이 불가능한 숫자들은 계산하면서 동시에 자리수를 고려하며 계산한 뒤 적절한 위치에 넣어주면 됐다. 자리수를 파악하는 것은 곱셈의 특성을 알면 쉽다.
